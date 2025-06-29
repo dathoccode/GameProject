2b358f487182f2e7c0b59d6f7df8909cc143ca69
dathoccode <datphungkl@gmail.com>
2025-06-29 22:26:14 +0700

    add tileset. joystick controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Joystick joystick;
+    public float moveSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = joystick.Direction;
+        transform.Translate(direction * Time.deltaTime * moveSpeed);
     }
 }
---
+++ Assets/Scripts/UI/Joystick.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    //
+    private RectTransform background;
+    private RectTransform handle;
+    private Vector2 inputVector;
+
+    // properties
+    public float Horizontal => inputVector.x;
+    public float Vertical => inputVector.y;
+    public Vector2 Direction => inputVector;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        // Convert the screen point to a local point in the background RectTransform so the handle can move relative to its background
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            background, eventData.position, eventData.pressEventCamera, out pos);
+
+        //Clamp the length of the vector (of the handle) to the radius of the background minus the radius of the handle because we don't want the handle to go outside the background
+        pos = Vector2.ClampMagnitude(pos, background.sizeDelta.x / 2 - handle.sizeDelta.x);
+
+        //move the handle to the new position
+        handle.anchoredPosition = pos;
+
+        //set the input vector to use later
+        inputVector = pos / (background.sizeDelta.x / 2 - handle.sizeDelta.x);
+    }
+
+    public void OnPointerDown(PointerEventData eventData) => OnDrag(eventData);
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // Reset the handle position and input vector when the pointer is released
+        handle.anchoredPosition = Vector2.zero;
+        inputVector = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<RectTransform>();
+        handle = transform.Find("JoystickHandle").GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
